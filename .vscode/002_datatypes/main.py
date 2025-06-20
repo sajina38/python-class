19bbaf5de2a69ce5871d7d0e33c30c9a6de07bdc
sajina38 <gurungsajina90@gmail.com>
2025-06-20 09:53:11 +0545

    Datatypes

---
+++ .vscode/002_datatypes/main.py
@@ -0,0 +1,44 @@
+x = 5               # int
+pi = 3.14           # float
+z = 2 + 3j          # complex
+name = "Anish"      # str
+colors = ["red", "blue"]  # list
+point = (4, 5)      # tuple
+student = {"name": "Ram", "age": 20}  # dict
+fruits = {"apple", "banana"}  # set
+is_active = True    # bool
+data = b"binary"    # bytes
+
+
+# snacks = ['Roti', 'Biscuits', 'Chips', 'noodles', ['Ram', 'Shyam']]
+# print(snacks[4][1])
+
+# personal_details = {
+#     'name': "Ram Sharma",
+#     'address': "Pokhara"
+# }
+# print (personal_details['address'])
+
+
+# data = [ 1,2,
+#     {
+#         'name': "Ram Sharma",
+#         'hobbies':["Coding"]
+#     }
+# ]
+ 
+# print (data[2]['hobbies'][0])
+
+
+# data = [
+#     {
+#         'name': "Ram Sharma",
+#         'hobbies':["Coding",
+#          {
+#         'interest': ["programming"]
+#         }]
+#     }
+# ]
+ 
+# print (data[0]['hobbies'] [1] ['interest'] [0])
+
