c228b768f30dd9792ea0160bb16e99e93a2e713e
sajina38 <gurungsajina90@gmail.com>
2025-06-20 09:44:07 +0545

    Basics

---
+++ 001_basics/main.py
@@ -0,0 +1,21 @@
+# print ('hello world')
+
+# a = 'Sajina'
+
+# print (a)
+
+name = input("Enter your name: ")
+location = input("Enter your location: ")
+phone_number = input("Enter your phone number: ")
+
+print(f""" Enter your {name},
+       Enter your {location},
+       Enter your {phone_number},
+       Welcome to Swift Academy
+       """
+       )
+
+
+# """ le multi line lekhna milcha
+# f le user input store garcha
+#
